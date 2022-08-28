021ecf167f7f8069ecae5f96b2dd54ab719f6663
Irik Kamaldinov <irik.frost@mail.ru>
2022-08-29 02:53:29 +0300

    добавили решение задачи 1

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Принимает на вход трехзначное число, и на выходе показывает втору. цифру этого числа
+Console.WriteLine("Введите трехзначное число:");
+int number1=Convert.ToInt32(Console.ReadLine());
+int number2=number1/10;
+int number3=number2%10;
+Console.WriteLine(number3); 
